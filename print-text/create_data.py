6b4536b6ad834faadbba730f04641ef9ecd64920
Bilal <abusaadb@post.bgu.ac.il>
2016-03-30 00:28:03 +0300

    creating data for pretraining

--- print-text/create_data.py
+++ print-text/create_data.py
@@ -5,6 +5,7 @@ from bidi.algorithm import get_display
 import arabic_reshaper
 import os
 from scipy.misc import imread, imsave
+import itertools
 import image_functions
 
 FONT_PREFIX= "/Library/Fonts/"
@@ -15,23 +16,27 @@ FONTS = ["Arial Unicode.ttf",
          "Damascus.tff",
          "DecoTypeNash.tff",
          "Nadeem.ttc"]
+DEFAULT_IMAGE_SIZES = [(100, 100)]
+DEFAULT_FONT_SIZES = [30, 40, 50]
+DEFAULT_LOCATIONS = [(25, 25)] # this should be probably be dependant on sizes 
 
-
-#here we want to create data for the words in words.txt
-#for each word in words.txt we want to createa directory with ~100 images
-def create_images(label,count=100):
-    # here we want to create count images of label with 
-    # different filters/rotations etc..
+# here we want to create data for the words in words.txt
+# for each word in words.txt we want to create a directory with ~100 images
+def create_images(label,count=100,font_size = 17):
+    #  here we want to create count images of label with 
+    #  different filters/rotations etc..
     img = Image.new("L", (100, 100),200)
     draw=ImageDraw.Draw(img)
-    font=ImageFont.truetype("/Library/Fonts/Arial Unicode.ttf",30)
+    font=ImageFont.truetype("/Library/Fonts/Arial Unicode.ttf", font_size)
     label_correct = arabic_reshaper.reshape(label)
     label_correct = get_display(label_correct)
     draw.text((22,22),label_correct,font=font)
     img.save('pics/' +label + '.png')
     img.close()
 
-
+# direc + org = path to the picture
+# this functions, reads the picture in path and creates different versions of it
+# using the functions in image_functions.get()
 def create_copies(direc, org):
     image_data = imread(direc+org).astype(np.float32)
     prefix = direc+'/test_out'
@@ -42,10 +47,13 @@ def create_copies(direc, org):
         for im in ims:
             imsave(prefix + str(index) +'.png', im)
             index+=1
-def create_data(word, label,fontsize = 50, loc = (25,25) ):
+
+# creates a number of images from a single image.
+# this function does not take close the picture.
+def process_single_image(word, label, size, fontsize = 50, loc = (25,25)):
     if not os.path.exists('pics/' + label):
         os.makedirs('pics/' + label)
-    img = Image.new("L", (100, 100),256)
+    img = Image.new("L", size ,256) # greyscale and white background
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("/Library/Fonts/Arial Unicode.ttf", fontsize)
     label_correct = arabic_reshaper.reshape(word)
@@ -54,14 +62,25 @@ def create_data(word, label,fontsize = 50, loc = (25,25) ):
     direc = 'pics/'+label
     img.save(direc + '/original.png')
     img.close()
-    #now we've created the base picture - now we can process it a bit more
+    # now we've created the base picture - now we can process it a bit more
     create_copies(direc, '/original.png')
-
     
+def create_data(word, label, sizes = DEFAULT_IMAGE_SIZES,
+        font_sizes = DEFAULT_FONT_SIZES,
+        locations = DEFAULT_LOCATIONS,
+        default_dir = 'pics/'):
+    i = 0
+    for (size, font_size, location) in itertools.product(
+            sizes, font_sizes, locations):
+        print i
+        process_single_image(word, label, size, font_size, location)
+        i += 1
+
 def process_word(line,f=create_data):
     line = line.split()
     word, label = unicode(line[0], 'utf-8'), line[1].rstrip()
     f(word,label)
+
 def create_words(f = 'words_and_labels.txt', process=process_word):
     words=open(f, 'r');
     for line in words:
